ba4d5138cbec24400c44530589ae989586813249
符梓璇 <110542733+FuEafan@users.noreply.github.com>
2023-07-12 14:32:21 +0800

    Update views.py

--- code/zhixuan/web_zhixuan/views.py
+++ code/zhixuan/web_zhixuan/views.py
@@ -11,32 +11,8 @@ def blog(request):
     return render(request, "blog.html")
 
 
-def blog_single(request):
-    return render(request, "blog-single.html")
-
-
-def contact(request):
-    return render(request, "contact.html")
-
-
-def course_grid2(request):
-    return render(request, "course-grid-2.html")
-
-
-def course_grid3(request):
-    return render(request, "course-grid-3.html")
-
-
-def course_grid4(request):
-    return render(request, "course-grid-4.html")
-
-
 def index(request):
     return render(request, "index.html")
-
-
-def pricing(request):
-    return render(request, "pricing.html")
 
 
 def teachers(request):
