d88e7749920c21d9f8135d54fc62ca041e576288
Bianca-josh <139028657+Bianca-josh@users.noreply.github.com>
2023-07-13 09:36:37 +0800

    Update views.py

--- code/zhixuan/web_zhixuan/views.py
+++ code/zhixuan/web_zhixuan/views.py
@@ -17,3 +17,16 @@ def index(request):
 
 def teachers(request):
     return render(request, "teachers.html")
+
+def updateinfo(request):
+    if request.method == 'POST':
+        # img = request.FILES.get('photo')
+        # user = request.FILES.get('photo').name
+        new_img = models.zhixuanDB(
+            photo=request.FILES.get('photo'),  # 拿到图片
+           # user=request.FILES.get('photo').name # 拿到图片的名字
+        )
+        new_img.save()  # 保存图片
+        return HttpResponse('上传成功！')
+
+    return render(request, 'blog-single.html')
